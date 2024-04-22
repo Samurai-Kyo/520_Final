a7279e887905f7bb71e8de61593f7e39b3e1326c
nisa <n.santos2619@gmail.com>
2024-04-22 18:45:38 -0400

    layout and home

---
+++ front_end/src/routes/home/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+	let tabNumber = 0;
+</script>
+
+<section class="card text-token w-full p-4">
+	<TabGroup>
+		<!-- Tabs -->
+		<Tab bind:group={tabNumber} name="books" value={0}>Books</Tab>
+		<Tab bind:group={tabNumber} name="movies" value={1}>Movies</Tab>
+		<Tab bind:group={tabNumber} name="tv" value={2}>Television</Tab>
+		<!-- Panel -->
+		<svelte:fragment slot="panel"></svelte:fragment>
+	</TabGroup>
+</section>
