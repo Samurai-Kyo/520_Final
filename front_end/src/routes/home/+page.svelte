c529c908287e6782c5bc99e3ef15dbe6fb7283b9
n.santos2619 <n.santos2619@gmail.com>
2024-04-23 19:41:44 -0400

    added tabs

--- front_end/src/routes/home/+page.svelte
+++ front_end/src/routes/home/+page.svelte
@@ -1,15 +1,36 @@
 <script>
-	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
-	let tabNumber = 0;
+  import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+  import CodeSummComponent from '$lib/components/CodeSummComponent.svelte';
+  import LoginFields from '$lib/components/LoginFields.svelte';
+  
+  let currentTab = 0;
 </script>
 
-<section class="card text-token w-full p-4">
-	<TabGroup>
-		<!-- Tabs -->
-		<Tab bind:group={tabNumber} name="books" value={0}>Books</Tab>
-		<Tab bind:group={tabNumber} name="movies" value={1}>Movies</Tab>
-		<Tab bind:group={tabNumber} name="tv" value={2}>Television</Tab>
-		<!-- Panel -->
-		<svelte:fragment slot="panel"></svelte:fragment>
-	</TabGroup>
-</section>
+
+
+<TabGroup>
+  <Tab bind:group={currentTab} name="Summarizer" value ={0}> Summarizer </Tab>
+  <Tab bind:group={currentTab} name="fileUploader" value ={1}>Upload File</Tab>
+	<svelte:fragment slot="panel">
+		{#if currentTab === 0}
+      <CodeSummComponent/>
+		{:else if currentTab === 1}
+      <LoginFields/>
+		{/if}
+	</svelte:fragment>
+
+
+</TabGroup>
+
+
+
+
+
+
+
+
+
+
+
+
+
