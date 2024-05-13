09cc1fbd9ea5ae35177ee4046468cde3893727c7
Nicholas Santos <89942411+nsantos19@users.noreply.github.com>
2024-05-12 22:03:16 -0400

    testing user functions

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import test from 'ava'
 
 let adminToken;
+let testUserToken;
 
 test.serial("ava is setup", t => {
   t.pass();
@@ -24,8 +25,7 @@ test.serial('/login admin login', async t => {
   t.is(data.firstTimeAdmin, true);
 })
 
-//login tests
-test.serial('/login token correctly invalidates', async t => {
+test.serial('/login token correctly invalidates token', async t => {
 
   const response = await fetch(`http://localhost:3000/login`, {
     method: 'GET',
@@ -70,6 +70,31 @@ test.serial('/login Wrong Password', async t => {
   t.is(response.status, 401);
 })
 
+test.serial('/login no username', async t => {
+
+  const response = await fetch(`http://localhost:3000/login`, {
+    method: 'GET',
+    headers: {
+      username: "",
+      password: "admin123123123123123123123"
+    }
+  });
+
+  t.is(response.status, 401);
+})
+test.serial('/login no password', async t => {
+
+  const response = await fetch(`http://localhost:3000/login`, {
+    method: 'GET',
+    headers: {
+      username: "admin",
+      password: ""
+    }
+  });
+
+  t.is(response.status, 401);
+})
+
 
 
 test.serial('/creatAccount succesfull account creation', async t => {
@@ -105,7 +130,7 @@ test.serial('/createAccount login with new Account ', async t => {
 })
 
 
-test.serial('/creatAccount username already exists', async t => {
+test.serial('/createAccount username already exists', async t => {
   const response = await fetch('http://localhost:3000/createAccount/', {
     method: 'GET',
     headers: {
@@ -120,23 +145,36 @@ test.serial('/creatAccount username already exists', async t => {
 
 })
 
-
-
-test.serial('/deleteUser delete testUser', async t => {
-
-  const response = await fetch(`http://localhost:3000/deleteUser`, {
+test.serial('/createAccount no username', async t => {
+  const response = await fetch('http://localhost:3000/createAccount/', {
     method: 'GET',
     headers: {
-      username: "admin",
-      token: adminToken,
-      toDelete: "testuser"
+      newUserName: "",
+      newPassword: "admin",
+      token: "",
+      newAdmin: 'false'
     }
   });
 
-  t.is(response.ok, true);
-
+  t.is(response.status, 403);
 
 })
+
+test.serial('/createAccount no password', async t => {
+  const response = await fetch('http://localhost:3000/createAccount/', {
+    method: 'GET',
+    headers: {
+      newUserName: "admin",
+      newPassword: "",
+      token: "",
+      newAdmin: 'false'
+    }
+  });
+
+  t.is(response.status, 403);
+
+})
+
 
 
 test.serial('/checkAdmin admin account succesfully validates', async t => {
@@ -153,7 +191,9 @@ test.serial('/checkAdmin admin account succesfully validates', async t => {
 
 })
 
+
 test.serial('/checkAdmin account fails to validate', async t => {
+
 
   const response = await fetch(`http://localhost:3000/checkAdmin`, {
     method: 'GET',
@@ -165,11 +205,212 @@ test.serial('/checkAdmin account fails to validate', async t => {
   const data = await response.json();
   t.is(data.isAdmin, false);
 
+});
+
+test.serial('/checkAdmin no account', async t => {
+
+
+  const response = await fetch(`http://localhost:3000/checkAdmin`, {
+    method: 'GET',
+    headers: {
+      username: "",
+      token: adminToken
+    }
+  });
+  const data = await response.json();
+  t.is(data.isAdmin, false);
+
+});
+
+test.serial('/checkAdmin no token', async t => {
+
+
+  const response = await fetch(`http://localhost:3000/checkAdmin`, {
+    method: 'GET',
+    headers: {
+      username: "",
+      token: ""
+    }
+  });
+  const data = await response.json();
+  t.is(data.isAdmin, false);
+
+});
+
+
+test.serial('/makeAdmin succesfully makes admin', async t => {
+  const response = await fetch(`http://localhost:3000/makeAdmin`, {
+    method: 'POST',
+    headers: {
+      username: "admin",
+      token: adminToken,
+      adminName: "testUser"
+    }
+  });
+  t.is(response.ok, true);
+});
+//
+//
+test.serial('/makeAdmin admin account succesfully validates', async t => {
+
+  const response = await fetch(`http://localhost:3000/login`, {
+    method: 'GET',
+    headers: {
+      username: "testuser",
+      password: "testuser"
+    }
+  });
+
+  const data = await response.json();
+  let newAdminToken = data.token;
+
+  const response2 = await fetch(`http://localhost:3000/checkAdmin`, {
+    method: 'GET',
+    headers: {
+      username: "testuser",
+      token: newAdminToken
+    }
+  });
+  const data2 = await response2.json();
+  t.is(data2.isAdmin, true);
+
+})
+//
+test.serial('/makeAdmin fails with bad admin token', async t => {
+  const response = await fetch(`http://localhost:3000/makeAdmin`, {
+    method: 'POST',
+    headers: {
+      username: "admin",
+      token: "",
+      adminName: "testuser"
+    }
+  });
+  t.is(response.status, 403);
+});
+
+test.serial('/makeAdmin empty username', async t => {
+  const response = await fetch(`http://localhost:3000/makeAdmin`, {
+    method: 'POST',
+    headers: {
+      username: "",
+      token: adminToken,
+      adminName: "testuser"
+    }
+  });
+  t.is(response.status, 403);
+});
+
+
+test.serial('/changePassword admin succesfully changes password', async t => {
+
+  const response = await fetch(`http://localhost:3000/changePassword`, {
+    method: 'GET',
+    headers: {
+      username: "admin",
+      token: adminToken,
+      newPassword: "testpassword",
+      newUsername: "testuser"
+    }
+  });
+  t.is(response.ok, true);
+  const response2 = await fetch(`http://localhost:3000/login`, {
+    method: 'GET',
+    headers: {
+      username: "testuser",
+      password: "testpassword"
+    }
+  });
+
+  const data = await response2.json();
+  testUserToken = data.token
+  t.is(response2.ok, true);
 })
 
+test.serial('/changePassword non admin succesfully changes password', async t => {
+
+  const response = await fetch(`http://localhost:3000/changePassword`, {
+    method: 'GET',
+    headers: {
+      username: "testuser",
+      token: testUserToken,
+      newPassword: "testpassword1",
+      newUsername: "testuser"
+    }
+  });
+
+  t.is(response.ok, true);
+
+  const response2 = await fetch(`http://localhost:3000/login`, {
+    method: 'GET',
+    headers: {
+      username: "testuser",
+      password: "testpassword1"
+    }
+  });
+
+  t.is(response2.ok, true);
 
 
 
 
+})
+test.serial('/changePassword fails with empty username', async t => {
+
+  const response = await fetch(`http://localhost:3000/changePassword`, {
+    method: 'GET',
+    headers: {
+      username: "admin",
+      token: adminToken,
+      newPassword: "testpassword",
+      newUsername: ""
+    }
+  });
+  t.is(response.status, 403);
+})
+test.serial('/changePassword fails with empty password', async t => {
+
+  const response = await fetch(`http://localhost:3000/changePassword`, {
+    method: 'GET',
+    headers: {
+      username: "admin",
+      token: adminToken,
+      newPassword: "",
+      newUsername: "testuser"
+    }
+  });
+  t.is(response.status, 403);
+})
+//
+//
+//
+test.serial('/deleteUser fails if token is not correct', async t => {
+
+  const response = await fetch(`http://localhost:3000/deleteUser`, {
+    method: 'GET',
+    headers: {
+      username: "admin",
+      token: "",
+      toDelete: "admin"
+    }
+  });
+
+  t.is(response.status, 403);
+});
+
+
+test.serial('/deleteUser delete testUser', async t => {
+
+  const response = await fetch(`http://localhost:3000/deleteUser`, {
+    method: 'GET',
+    headers: {
+      username: "admin",
+      token: adminToken,
+      toDelete: "testuser"
+    }
+  });
+
+  t.is(response.ok, true);
+
+})
 
 
